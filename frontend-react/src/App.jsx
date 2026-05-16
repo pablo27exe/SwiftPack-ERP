23359b79032f4579af12a0ddf41655110d293319
pablo27exe <id22028@dasc.edu.mx>
2026-05-16 11:48:42 -0600

    Fix App.jsx

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
 import { AuthProvider } from './context/AuthContext'
+import { EnvioProvider } from './context/EnvioContext'
 import AppRoutes from './routes/AppRoutes'
 
 function App() {
   return (
     <AuthProvider>
-      <AppRoutes />
+      <EnvioProvider>
+                <AppRoutes />
+      </EnvioProvider>
     </AuthProvider>
   )
 }
